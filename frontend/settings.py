b2c6390a2de763c41d70ec80b349a08513a0bb7f
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-05-14 21:21:55 -0300

    adding static files configuration

--- frontend/settings.py
+++ frontend/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/static"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
