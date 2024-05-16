5c16d48cabeab57c45e42e763b854f4cbbd11f7c
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-05-16 10:06:52 -0300

    altering model decimal fields and str functions

--- frontend/finmanager/models.py
+++ frontend/finmanager/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class BitcoinAddress(models.Model):
     id = models.AutoField(primary_key=True)
     address = models.CharField(max_length=42, unique=True)
-    balance = models.DecimalField(max_digits=20, decimal_places=8, default=0)
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     brl_balance = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -16,9 +16,9 @@ class BitcoinAddress(models.Model):
 class EthereumAddress(models.Model):
     id = models.AutoField(primary_key=True)
     address = models.CharField(max_length=42, unique=True)
-    balance = models.DecimalField(max_digits=20, decimal_places=8, default=0)
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     brl_balance = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -29,42 +29,42 @@ class Stock(models.Model):
     id = models.AutoField(primary_key=True)
     negotiation_code = models.CharField(max_length=10, unique=True)
     quantity = models.IntegerField(default=0)
-    price = models.DecimalField(max_digits=20, decimal_places=8, default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     last_update = models.DateTimeField(auto_now=True)
     updated_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
 
     def __str__(self):
-        return self.symbol
+        return self.negotiation_code
 
 
 class Fii(models.Model):
     id = models.AutoField(primary_key=True)
     negotiation_code = models.CharField(max_length=10, unique=True)
     quantity = models.IntegerField(default=0)
-    price = models.DecimalField(max_digits=20, decimal_places=8, default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     last_update = models.DateTimeField(auto_now=True)
     updated_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
 
     def __str__(self):
-        return self.symbol
+        return self.negotiation_code
 
 
 class TreasuryDirect(models.Model):
     id = models.AutoField(primary_key=True)
     product = models.CharField(max_length=10, unique=True)
     indexer = models.CharField(max_length=50)
-    quantity = models.DecimalField(max_digits=20, decimal_places=8, default=0)
-    deadline = models.DateTimeField()
+    quantity = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    deadline = models.CharField(max_length=20, null=True)
     applied_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     brute_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     liquid_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     updated_value = models.DecimalField(
-        max_digits=20, decimal_places=8, default=0)
+        max_digits=20, decimal_places=2, default=0)
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
