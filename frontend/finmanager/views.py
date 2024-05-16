9544d3684436dabeb756d7d002d494be7b21cfb4
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-05-16 09:08:24 -0300

    updating views req to get brl balance of crypto

--- frontend/finmanager/views.py
+++ frontend/finmanager/views.py
@@ -24,7 +24,6 @@ def index(request):
             btc_balance = btc(btc_address)
             eth_balance = eth(eth_address)
             b3_parsed = b3(b3_file)
-            print(btc_balance, eth_balance)
 
             ctx = {
                 'btc_balance': btc_balance,
@@ -34,7 +33,7 @@ def index(request):
             return render(request, 'dashboard.html', ctx)
         else:
             return render(request, 'form.html', {'form': form, 'errors': form.errors})
-        
+
     form = DashboardForm()
     return render(request, 'form.html', {'form': form})
 
@@ -46,27 +45,58 @@ HEADERS = {
 
 BASE_URL = "http://localhost:8000"
 
+
 def btc(address: str) -> BitcoinAddress:
-    response = requests.get(f"{BASE_URL}/bitcoin/balance/{address}", headers=HEADERS)
+    response = requests.get(
+        f"{BASE_URL}/bitcoin/balance/{address}", headers=HEADERS)
+
+    price = 0
+    btc_val = requests.get(f"{BASE_URL}/bitcoin/brl-price", headers=HEADERS)
+    btc_val_json = json.loads(btc_val.json())
+    price = btc_val_json.get("price")
+
     if response.status_code == 200:
         data = json.loads(response.json())
         balance = data.get("balance")
-        btc_balance = BitcoinAddress.objects.get_or_create(address=address, balance=balance)
-        return btc_balance
+        btc_price = price * balance
+
+        btc_address, created = BitcoinAddress.objects.get_or_create(
+            address=address)
+        btc_address.balance = balance
+        btc_address.brl_balance = btc_price
+        btc_address.save()
+
+        return btc_address
     return
 
+
 def eth(address: str) -> EthereumAddress:
-    response = requests.get(f"{BASE_URL}/ethereum/balance/{address}", headers=HEADERS)
+    response = requests.get(
+        f"{BASE_URL}/ethereum/balance/{address}", headers=HEADERS)
+
+    eth_val = requests.get(f"{BASE_URL}/ethereum/brl-price", headers=HEADERS)
+    eth_val_json = json.loads(eth_val.json())
+    price = eth_val_json.get("price")
+
     if response.status_code == 200:
         data = json.loads(response.json())
         balance = data.get("balance")
-        eth_balance = EthereumAddress.objects.get_or_create(address=address, balance=balance)
-        return eth_balance
+        eth_price = price * balance
+
+        eth_address, created = EthereumAddress.objects.get_or_create(
+            address=address
+        )
+        eth_address.balance = balance
+        eth_address.brl_balance = eth_price
+        eth_address.save()
+        return eth_address
     return
+
 
 def b3(b3_file):
     files = {'file': b3_file}
-    response = requests.post(f"{BASE_URL}/b3/parse", headers=HEADERS, files=files)
+    response = requests.post(f"{BASE_URL}/b3/parse",
+                             headers=HEADERS, files=files)
     if response.status_code == 200:
         data = json.loads(response.json())
         return data
